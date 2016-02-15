5510ced0016b0323ffc6c085878cd01858c363b7
aundra miller and matt knutson <miller.aundra@gmail.com>
2016-02-15 10:02:12 -0800

    tests pass, it will return ping, pong or pingpong as appropriate.

--- src/PingPongGenerator.php
+++ src/PingPongGenerator.php
@@ -6,11 +6,21 @@
         {
             $ping_pong_array = array();
             for ($i=1; $i <= $input_number; $i++) {
-                array_push($ping_pong_array, $i);
+                if ($i % 3 == 0 && $i % 5 ==0) {
+                    array_push($ping_pong_array, 'ping-pong');
+                }
+                elseif ($i % 5 == 0) {
+                    array_push($ping_pong_array, 'pong');
+                }
+                elseif ($i % 3 == 0) {
+                    array_push($ping_pong_array, 'ping');
+                }
+                else {
+                    array_push($ping_pong_array, $i);
+                }
             }
             return $ping_pong_array;
         }
     }
-
 
  ?>
--- tests/PingPongGeneratorTest.php
+++ tests/PingPongGeneratorTest.php
@@ -12,9 +12,36 @@
             $result = $test_PingPongGenerator->makePingPong($input_number);
 
             $this->assertEquals(array(1, 2), $result);
-
-
         }
 
+        function test_createArray_withPing()
+        {
+            $test_PingPongGenerator = new PingPongGenerator;
+            $input_number = 3;
+
+            $result = $test_PingPongGenerator->makePingPong($input_number);
+
+            $this->assertEquals(array(1, 2, 'ping'), $result);
+        }
+
+        function test_createArray_withPong()
+        {
+            $test_PingPongGenerator = new PingPongGenerator;
+            $input_number = 5;
+
+            $result = $test_PingPongGenerator->makePingPong($input_number);
+
+            $this->assertEquals(array(1, 2, 'ping', 4, 'pong'), $result);
+        }
+
+        function test_createArray_withPingPong()
+        {
+            $test_PingPongGenerator = new PingPongGenerator;
+            $input_number = 15;
+
+            $result = $test_PingPongGenerator->makePingPong($input_number);
+
+            $this->assertEquals(array(1, 2, 'ping', 4, 'pong', 'ping', 7, 8, 'ping', 'pong', 11, 'ping', 13, 14, 'ping-pong'), $result);
+        }
     }
 ?>
